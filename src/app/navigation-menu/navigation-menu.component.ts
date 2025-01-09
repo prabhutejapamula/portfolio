b2605d8a0f89bb525efb40354790e17ffba0cd43
teja137 <ajetdream@yahoo.com>
2025-01-09 21:03:51 +0530

    refactor: added more colors for banner

--- src/app/navigation-menu/navigation-menu.component.ts
+++ src/app/navigation-menu/navigation-menu.component.ts
@@ -8,7 +8,18 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrl: './navigation-menu.component.scss'
 })
 export class NavigationMenuComponent implements OnInit {
-  colors = ['#ff00b3', '#00ffcc', '#ffcc00', '#ff0066', '#cc00ff'];
+  colors = [
+    '#ff00b3', '#00ffcc', '#ffcc00', '#ff0066', '#cc00ff',
+    '#ff0033', '#ccff00', '#66ccff', '#ff6600', '#00ccff',
+    '#ffcc99', '#ff3333', '#33ff66', '#9966cc', '#ff3399',
+    '#6699cc', '#ff6666', '#66ff33', '#3399ff', '#ff99cc',
+    '#ff0033', '#cc33ff', '#ff6600', '#33ccff', '#66ffcc',
+    '#996699', '#ffcc33', '#00ff33', '#ff3300', '#ff0099',
+    '#0099cc', '#ff3333', '#00cc33', '#33cc99', '#ff9933',
+    '#cc99cc', '#66ff99', '#ff3399', '#cc0066', '#ff66cc',
+    '#cc66cc', '#6600ff', '#33cc33', '#ff6600', '#ff0000',
+    '#3399cc', '#00ccff', '#ccff66', '#ff66ff', '#ff0033',
+  ];
   resumeImage: string = "assets/cv.png";
   linkedImage: string = "assets/linkedin.png";
   leetcodeImage: string = "assets/LeetCode.png";
